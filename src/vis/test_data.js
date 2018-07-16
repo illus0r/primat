6ffe88bbbe3cd60c76613c2f3687a099a41e9120
MainOlma <mainolma@gmail.com>
2018-07-16 17:06:52 +0300

    research: find leaves and min/max period

---
+++ src/vis/test_data.js
@@ -0,0 +1,144 @@
+[
+    {
+        "key": "",
+        "values": [
+            {
+                "key": "SCANDENTIA",
+                "values": [
+                    {
+                        "key": "",
+                        "values": [
+                            {
+                                "key": "",
+                                "values": [
+                                    {
+                                        "key": "",
+                                        "values": [
+                                            {
+                                                "key": "",
+                                                "values": [
+                                                    {
+                                                        "key": "",
+                                                        "values": [
+                                                            {
+                                                                "key": "Tupaildae",
+                                                                "values": [
+                                                                    {
+                                                                        "key": "Ptilocercinae",
+                                                                        "values": [
+                                                                            {
+                                                                                "key": "Eodendrogale parvum",
+                                                                                "values": [
+                                                                                    {
+                                                                                        "id": "1",
+                                                                                        "mir": "",
+                                                                                        "order": "SCANDENTIA",
+                                                                                        "suborder": "",
+                                                                                        "hyporder": "",
+                                                                                        "infraorder": "",
+                                                                                        "parvorder": "",
+                                                                                        "superfamily": "",
+                                                                                        "family": "Tupaildae",
+                                                                                        "subfamily": "Ptilocercinae",
+                                                                                        "genus": "Eodendrogale parvum",
+                                                                                        "location": "Китай"
+                                                                                    }
+                                                                                ]
+                                                                            },
+                                                                            {
+                                                                                "key": "—",
+                                                                                "values": [
+                                                                                    {
+                                                                                        "id": "2",
+                                                                                        "mir": "",
+                                                                                        "order": "SCANDENTIA",
+                                                                                        "suborder": "",
+                                                                                        "hyporder": "",
+                                                                                        "infraorder": "",
+                                                                                        "parvorder": "",
+                                                                                        "superfamily": "",
+                                                                                        "family": "Tupaildae",
+                                                                                        "subfamily": "Ptilocercinae",
+                                                                                        "genus": "—",
+                                                                                        "location": ""
+                                                                                    }
+                                                                                ]
+                                                                            }
+                                                                        ]
+                                                                    },
+                                                                    {
+                                                                        "key": "Tupaiinae",
+                                                                        "values": [
+                                                                            {
+                                                                                "key": "Tupaia miocenica",
+                                                                                "values": [
+                                                                                    {
+                                                                                        "id": "3",
+                                                                                        "mir": "",
+                                                                                        "order": "SCANDENTIA",
+                                                                                        "suborder": "",
+                                                                                        "hyporder": "",
+                                                                                        "infraorder": "",
+                                                                                        "parvorder": "",
+                                                                                        "superfamily": "",
+                                                                                        "family": "Tupaildae",
+                                                                                        "subfamily": "Tupaiinae",
+                                                                                        "genus": "Tupaia miocenica",
+                                                                                        "location": "Таиланд"
+                                                                                    }
+                                                                                ]
+                                                                            },
+                                                                            {
+                                                                                "key": "Tupaia sivalicus",
+                                                                                "values": [
+                                                                                    {
+                                                                                        "id": "4",
+                                                                                        "mir": "",
+                                                                                        "order": "SCANDENTIA",
+                                                                                        "suborder": "",
+                                                                                        "hyporder": "",
+                                                                                        "infraorder": "",
+                                                                                        "parvorder": "",
+                                                                                        "superfamily": "",
+                                                                                        "family": "Tupaildae",
+                                                                                        "subfamily": "Tupaiinae",
+                                                                                        "genus": "Tupaia sivalicus",
+                                                                                        "location": "Индия"
+                                                                                    }
+                                                                                ]
+                                                                            },
+                                                                            {
+                                                                                "key": "Prodendrogale yunnanica",
+                                                                                "values": [
+                                                                                    {
+                                                                                        "id": "5",
+                                                                                        "mir": "",
+                                                                                        "order": "SCANDENTIA",
+                                                                                        "suborder": "",
+                                                                                        "hyporder": "",
+                                                                                        "infraorder": "",
+                                                                                        "parvorder": "",
+                                                                                        "superfamily": "",
+                                                                                        "family": "Tupaildae",
+                                                                                        "subfamily": "Tupaiinae",
+                                                                                        "genus": "Prodendrogale yunnanica",
+                                                                                        "location": "Китай"
+                                                                                    }
+                                                                                ]
+                                                                            },
+                                                                            {
+                                                                                "key": "—",
+                                                                                "values": [
+                                                                                    {
+                                                                                        "id": "6",
+                                                                                        "mir": "",
+                                                                                        "order": "SCANDENTIA",
+                                                                                        "suborder": "",
+                                                                                        "hyporder": "",
+                                                                                        "infraorder": "",
+                                                                                        "parvorder": "",
+                                                                                        "superfamily": "",
+                                                                                        "family": "Tupaildae",
+                                                                                        "subfamily": "Tupaiinae",
+                                                                                        "genus": "—",
+                …</pre>
